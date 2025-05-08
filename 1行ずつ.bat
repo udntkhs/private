d23a53173ae9b2fdfb2e8d8f0450b13d96378819
udntkhs <63333472+udntkhs@users.noreply.github.com>
2025-05-09 06:55:14 +0900

    Add files via upload

--- 1行ずつ.bat
+++ 1行ずつ.bat
@@ -1,12 +1,22 @@
 @echo off
+
+set FILE1=D:\Document\test1.txt
+set FILE2=D:\Document\test2.txt
 rem ファイルを1行ずつ読み込む
-for /f "tokens=*" %%a in ('type "D:\Document\test1.txt"') do (
-
-  rem 出力する
+for /f "tokens=*" %%a in ('type "%FILE1%"') do (
+ echo %FILE1%
   echo %%a
-
   rem 見やすいように区切りを出力する
   echo ---
+
+for /f "tokens=*" %%o in ('type "%FILE2%"') do (
+ echo %FILE2%
+  echo %%o
+  rem 見やすいように区切りを出力する
+  echo ---
+
+)
+
 
 )
 pause
